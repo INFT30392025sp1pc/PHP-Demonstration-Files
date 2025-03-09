8b6d75554e30b0bcb3544f80ca27909be2b6e21c
jmsgrdnr1 <jmsgrdnr1@gmail.com>
2025-03-09 12:43:50 +1000

    Update select_student_data.php

--- htdocs/php/select_student_data.php
+++ htdocs/php/select_student_data.php
@@ -1,6 +1,28 @@
-<?php $title = 'Select Student Data'; include("templates/top.php");?>
+<?php
+    $servername = "ls-d569aff0f012a672548dfa141c2ff6da29f4a426.cx4g6oyqyv3o.ap-southeast-2.rds.amazonaws.com";
+    $username = "dbmasteruser";
+    $password = ",urI{;P+E-97C<Is>,AZkgnDZ5V313mw";
+    $dbname = "dbmaster";
 
-    <!--This would be where you would make the changes that need to be made on each page-->
-    <?php include 'php/select_student_data.php';?>
-    
-<?php include("templates/bottom.php");?>
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT student_ID, first_name, last_name FROM student_data";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["student_ID"]."</td><td>".$row["first_name"]." ".$row["last_name"]."</td></tr>";
+    }
+    echo "</table>";
+    } else {
+    echo "0 results";
+    }
+    $conn->close();
+?>
